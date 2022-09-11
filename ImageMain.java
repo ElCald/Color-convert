d553da2672585f9367abd51ceaeb99be123722d9
ElCald <91151446+ElCald@users.noreply.github.com>
2022-09-11 16:07:59 +0200

    Update ImageMain.java

--- ImageMain.java
+++ ImageMain.java
@@ -1,3 +1,9 @@
+/**
+ * Convertisseur de couleur d'une image celon une palette selectionnee
+ * @author ElCald
+ * @version 3
+ */
+
 public class ImageMain{
 
     public static void main(String [] args){
